e1736a097d4fcee5c3db210d96bb0f6301daa58a
chamalanuradha <chamalanuradha98765@gmail.com>
2025-03-06 12:26:46 +0530

    create sub_cate

---
+++ server/models/subCategory.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const subCategorySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    image: { type: String, default: "" },
+    category: [{
+            type: mongoose.Schema.ObjectId,
+            ref: 'category'
+        }],
+}, {
+    timestamps: true 
+});
+
+const SubCategoryModel = mongoose.model('subCategory', subCategorySchema);
+
+export default SubCategoryModel;
