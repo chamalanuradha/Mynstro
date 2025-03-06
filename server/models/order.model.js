10b64b79d70152197f8ae1ab03eb06ef92f276b8
chamalanuradha <chamalanuradha98765@gmail.com>
2025-03-06 12:55:00 +0530

    create order model

---
+++ server/models/order.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.ObjectId, ref: 'user', required: true },
+    orderId: { type: String, required: true, unique: true },
+    productId: { type: mongoose.Schema.ObjectId, ref: 'product', required: true },
+    product_details: {
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+        quantity: { type: Number, required: true }
+    },
+    status: { type: String, enum: ["Pending", "Processing", "Shipped", "Delivered", "Cancelled"], default: "Pending" }
+}, {
+    timestamps: true 
+});
+
+const OrderModel = mongoose.model('order', orderSchema);
+
+export default OrderModel;
