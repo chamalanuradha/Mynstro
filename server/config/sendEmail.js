7b5fa715627080c34b30abfd79f5f13aaff258a7
chamalanuradha <chamalanuradha98765@gmail.com>
2025-03-06 20:13:06 +0530

    resend email configaration

---
+++ server/config/sendEmail.js
@@ -0,0 +1,28 @@
+import { Resend } from 'resend';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+if(!process.env.RESEND_API){
+    console.log('Not provide resend api to .env file')
+}
+
+const resend = new Resend(process.env.RESEND_API);
+
+const seneEmail = async (sendTo, subject, html) => {
+    try {
+        const { data, error } = await resend.emails.send({
+            from: 'Mynstro <onboarding@resend.dev>',
+            to: [sendTo],
+            subject: subject,
+            html: html,
+          });
+          if (error) {
+            return console.error({ error });
+          }
+    } catch (error) {
+        console.error({ error });
+    }
+    
+}
+export default seneEmail;
