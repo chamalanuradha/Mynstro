96be37af1301b793e489e31984dbe4df8a7409fe
chamalanuradha <chamalanuradha98765@gmail.com>
2025-03-10 20:04:12 +0530

    set login user routes

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
-import {registerUser, verifyEmail} from "../controllers/user.controller.js";
+import {registerUser, verifyEmail, login} from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 userRouter.post('/register', registerUser);
 userRouter.post('/verifyemail', verifyEmail);
+userRouter.post('/login', login);
 
 export default userRouter;
