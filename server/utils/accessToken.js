87ba5e20b1c03b54045c508a295ac3bfe53bf112
chamalanuradha <chamalanuradha98765@gmail.com>
2025-03-10 19:45:22 +0530

    access token and refresh token genarate functions

--- server/utils/accessToken.js
+++ server/utils/accessToken.js
@@ -5,7 +5,7 @@ dotenv.config();
 const genarateAccessToken = async (userId) => {
 
     const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '1d'
+        expiresIn: '3h'
     })
     return token
 
---
+++ server/utils/refreshToken.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import UserModel from '../models/user.model';
+dotenv.config();
+
+const genarateRefreshToken = async (userId) => {
+
+    const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '30d'
+    })
+
+    const  updateToken = await UserModel.updateOne(
+        { _id: userId }, 
+        { refreshToken: token })
+    return token
+
+}
+
+export default genarateRefreshToken
