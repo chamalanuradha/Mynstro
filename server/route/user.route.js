2d0539832d01ee3bfbcdff725e6a7fa9d1ca5c14
chamalanuradha <chamalanuradha98765@gmail.com>
2025-03-10 15:49:30 +0530

    set route to email verification

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
-import {registerUser} from "../controllers/user.controller.js";
+import {registerUser, verifyEmail} from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 userRouter.post('/register', registerUser);
+userRouter.post('/verifyemail', verifyEmail);
 
 export default userRouter;
