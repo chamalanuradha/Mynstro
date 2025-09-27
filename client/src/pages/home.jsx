a491a20fe1c73ec9159e386785b995fcf963c636
chamalanuradha <chamalanuradha98765@gmail.com>
2025-09-27 09:33:04 +0530

    dockerize frontend

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function home() {
   return (
     <div>
-       <h1 className='text-red-500 text-3xl font-bold underline'>Home</h1> 
+       <h1 className='text-red-500 text-3xl font-bold underline'>Home page</h1> 
     </div>
   )
 }
