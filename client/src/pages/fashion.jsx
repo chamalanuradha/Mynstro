828628b06098cae6be95ae10df7b2b9692f5f3ab
chamalanuradha <chamalanuradha98765@gmail.com>
2025-12-23 21:43:19 +0530

    define all pages nav link

---
+++ client/src/pages/fashion.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function fashion() {
+  return (
+    <div>fashion</div>
+  )
+}
---
+++ client/src/pages/jewellery.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function jewellery() {
+  return (
+    <div>jewellery</div>
+  )
+}
