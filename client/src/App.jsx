c4dbee9c18485df0b2e8d53849473a75cde3b096
chamalanuradha <chamalanuradha98765@gmail.com>
2025-12-23 21:15:26 +0530

    define aboutus and contact pages and set routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,17 @@ import Landing from "./pages/landing";
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
 import Home from "./pages/home";
+import Contact from "./pages/contact";
+import AboutUs from "./pages/aboutus";
 
-function App() {
+export default function App() {
   const location = useLocation();
 
-  const hideLayout = ["/login", "/register"].includes(location.pathname);
+  const noLayoutRoutes = ["/login", "/register"];
+  const validRoutes = ["/", "/login", "/register", "/home", "/contact", "/aboutus"];
+
+  const is404 = !validRoutes.includes(location.pathname);
+  const hideLayout = noLayoutRoutes.includes(location.pathname) || is404;
 
   return (
     <>
@@ -21,6 +27,9 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
       </main>
 
@@ -28,5 +37,3 @@ function App() {
     </>
   );
 }
-
-export default App;
